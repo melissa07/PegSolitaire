3955e368a74cd05878129ae33090d56ebc9b8c02
DESKTOP-5T68F8C\Christelle <adeolamelissasissoko95@gmail.com>
2017-10-04 17:16:36 -0400

    Travail sur linsertion dans le tab a deux dim a completer

--- src/LecteurPlateau.java
+++ src/LecteurPlateau.java
@@ -1,12 +1,13 @@
+import javax.sound.midi.Soundbank;
 import java.io.*;
 
 public class LecteurPlateau {
+    private static final int MAX_SIZE = 7;
 
     public static int[][] lirePlateau(String fileName) {
         try{
             FileInputStream fstream = new FileInputStream(fileName+".peg");
-            int[][] tabCases = fileReader(fstream);
-
+            fileReader(fstream);
 
         }
         catch(FileNotFoundException fnfe){
@@ -17,10 +18,24 @@ public class LecteurPlateau {
     }
 
     private static int[][] fileReader(FileInputStream fstream) throws FileNotFoundException {
-        int[][] tabCases = new int[7][7];
+        int[][] tabCases = new int[MAX_SIZE][MAX_SIZE];
         try{
             BufferedReader br = new BufferedReader(new InputStreamReader(fstream));
             String line = "";
+            int rangee = 0;
+            int colonne = 0;
+            while((line = br.readLine()) != null){
+                char[] lineInFile = line.toCharArray();
+                for (int i=0; i<lineInFile.length; i++) {
+                    tabCases[rangee][colonne] = (lineInFile[colonne]); //todo debug tab
+                    colonne++;
+                    if(colonne >= MAX_SIZE)
+                        colonne = 0;
+                    System.out.print(tabCases[rangee][colonne]);
+                }
+                rangee+=1;
+                System.out.println("\n");
+            }
 
         }
         catch (Exception e) {
--- src/PegSolitaire.java
+++ src/PegSolitaire.java
@@ -1,11 +1,11 @@
 public class PegSolitaire {
-    private static final String PUZZLE_NAME = "puzzle1.peg";
+    private static final String PUZZLE_NAME = "Puzzles/puzzle1";
 
 
     public static void main(String[] args) {
         int[][] tabCases = LecteurPlateau.lirePlateau(PUZZLE_NAME);
 
         Puzzle lePuzz = new Puzzle(tabCases);
-        System.out.println("Hello world!");
+//        System.out.println("Hello world!");
     }
 }
