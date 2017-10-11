a1c76741f6ebe6c618767a772e1aa9a2165be814
DESKTOP-5T68F8C\Christelle <adeolamelissasissoko95@gmail.com>
2017-10-11 14:14:43 -0400

    Creation du tableau de int

--- src/LecteurPlateau.java
+++ src/LecteurPlateau.java
@@ -1,13 +1,14 @@
 import javax.sound.midi.Soundbank;
 import java.io.*;
+import java.util.Scanner;
 
 public class LecteurPlateau {
     private static final int MAX_SIZE = 7;
 
     public static int[][] lirePlateau(String fileName) {
         try{
-            FileInputStream fstream = new FileInputStream(fileName+".peg");
-            fileReader(fstream);
+//            FileInputStream fstream = new FileInputStream(fileName+".peg");
+            fileReader(fileName);
 
         }
         catch(FileNotFoundException fnfe){
@@ -17,30 +18,33 @@ public class LecteurPlateau {
         return null;
     }
 
-    private static int[][] fileReader(FileInputStream fstream) throws FileNotFoundException {
+    private static int[][] fileReader(String fileName) throws FileNotFoundException {
         int[][] tabCases = new int[MAX_SIZE][MAX_SIZE];
-        try{
-            BufferedReader br = new BufferedReader(new InputStreamReader(fstream));
-            String line = "";
-            int rangee = 0;
-            int colonne = 0;
-            while((line = br.readLine()) != null){
-                char[] lineInFile = line.toCharArray();
-                for (int i=0; i<lineInFile.length; i++) {
-                    tabCases[rangee][colonne] = (lineInFile[colonne]); //todo debug tab
-                    colonne++;
-                    if(colonne >= MAX_SIZE)
-                        colonne = 0;
-                    System.out.print(tabCases[rangee][colonne]);
-                }
-                rangee+=1;
-                System.out.println("\n");
-            }
+        Scanner lineScanner = new Scanner(new File(fileName+".peg"));
+        Scanner numberScanner = null;
+        int tabLine = 0;
+        int tabColumn = 0;
 
+        while(lineScanner.hasNextLine()) { // ligne : 0
+            String line = lineScanner.nextLine();
+//            System.out.println(line);
+            numberScanner = new Scanner(line);
+            while(numberScanner.hasNext()) {
+                String token = numberScanner.next();
+                char[] tabToken = token.toCharArray();
+                for (int i =0; i< tabToken.length; i++) {
+                    tabCases[tabLine][tabColumn] = Character.getNumericValue(tabToken[i]);
+                    tabColumn += 1;
+                }
+                tabColumn = 0;
+            }
+            tabLine += 1;
         }
-        catch (Exception e) {
-            e.printStackTrace();
-        }
+        lineScanner.close();
+        numberScanner.close();
+
+
+
         return tabCases;
     }
 
--- src/PegSolitaire.java
+++ src/PegSolitaire.java
@@ -6,6 +6,14 @@ public class PegSolitaire {
         int[][] tabCases = LecteurPlateau.lirePlateau(PUZZLE_NAME);
 
         Puzzle lePuzz = new Puzzle(tabCases);
+        // O(n^2)
+        for(int i=0; i<= tabCases.length; i++) {
+            for(int j=0; j<= tabCases.length; i++) {
+                System.out.println("Nombre a la rangee " +i+ " et" +
+                        "a la colonne " + j+ ": " +tabCases[i][j]);
+            }
+            System.out.println("\n");
+        }
 //        System.out.println("Hello world!");
     }
 }
