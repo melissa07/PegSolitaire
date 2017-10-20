4ecf10559ac63bc588dc23adb6d5156cfb623bb4
Tristan Séguin-Chouinard <tri-23-sc@hotmail.ca>
2017-10-19 23:44:36 -0400

    Algo Fonctionel!

--- src/PegLogique.java
+++ src/PegLogique.java
@@ -1,13 +1,10 @@
 public class PegLogique {
-    private Direction d = Direction.NORD; // NORD, EST, SUD, OUEST
     private Puzzle p;
     private final int IS_EMPTY = 2;
     private final int IS_FILLED = 1;
     private final int OUT_OF_GAME = 0;
     private int total_pegs = 0;
     private int counter = 0;
-    private DeplacementsCakeTaker depl = new DeplacementsCakeTaker();
-//    private MementoDeplacement md = null;
 
     public PegLogique(Puzzle p) {
         this.p = p;
@@ -30,95 +27,65 @@ public class PegLogique {
             for(int curY = 0; curY < tabCases[curX].length; curY++){
                 for(int k = 0; k < 4; k++){
 
-                    if(isMovable(curX, curY, d)) {
+                    if(isMovable(curX, curY, k)) {
                         counter++;
                         //System.out.println("Peg is movable");
                         //deplacement (a mettre dans memento pour annuler si mauvais mouvement)
-                        switch (d) {
-                            case NORD:
+                        switch (k) {
+                            case 1:
                                 tabCases[curX][curY] = IS_FILLED;
                                 tabCases[curX][curY - 2] = IS_EMPTY;
                                 tabCases[curX][curY - 1] = IS_EMPTY;
-                                depl.add(new MementoDeplacement(curX, curY, Direction.NORD)); // todo est-ce que ce sont les bonnes coordonnes a enregistrer ?
                                 break;
-                            case EST:
+                            case 0:
                                 tabCases[curX][curY] = IS_FILLED;
                                 tabCases[curX + 2][curY] = IS_EMPTY;
                                 tabCases[curX + 1][curY] = IS_EMPTY;
-                                depl.add(new MementoDeplacement(curX, curY, Direction.EST)); // todo est-ce que ce sont les bonnes coordonnes a enregistrer ?
                                 break;
-                            case SUD:
+                            case 3:
                                 tabCases[curX][curY] = IS_FILLED;
                                 tabCases[curX][curY + 2] = IS_EMPTY;
                                 tabCases[curX][curY + 1] = IS_EMPTY;
-                                depl.add(new MementoDeplacement(curX, curY, Direction.SUD)); // todo est-ce que ce sont les bonnes coordonnes a enregistrer ?
                                 break;
-                            case OUEST:
+                            case 2:
                                 tabCases[curX][curY] = IS_FILLED;
                                 tabCases[curX - 2][curY] = IS_EMPTY;
                                 tabCases[curX - 1][curY] = IS_EMPTY;
-                                depl.add(new MementoDeplacement(curX, curY, Direction.OUEST)); // todo est-ce que ce sont les bonnes coordonnes a enregistrer ?
                                 break;
                         }
                         p.setTabCases(tabCases);
 
-                     /*
-                        for (int s=0; s<depl.getSize(); s++) {
-                            System.out.println("Deplacement: " +depl.get(s).getDirection() + " " + depl.get(s).getPosX()
-                                    + " " + depl.get(s).getPosY());
-                        }
-                    */
                         if (movePeg() == true) {
                             return true;
-                        }
+                        }else {
 
-                        if(depl.getSize() > 0) {
-                            curX = depl.get(depl.getSize() - 1).getPosX();
-                            curY = depl.get(depl.getSize() - 1).getPosY();
-                            d = depl.get(depl.getSize() - 1).getDirection();
-
-                            switch (d) {
-                                case NORD:
-                                    tabCases[curX][curY] = IS_EMPTY;
-                                    tabCases[curX][curY - 2] = IS_FILLED;
-                                    tabCases[curX][curY - 1] = IS_FILLED;
-                                    break;
-                                case EST:
-                                    tabCases[curX][curY] = IS_EMPTY;
-                                    tabCases[curX + 2][curY] = IS_FILLED;
-                                    tabCases[curX + 1][curY] = IS_FILLED;
-                                    break;
-                                case SUD:
-                                    tabCases[curX][curY] = IS_EMPTY;
-                                    tabCases[curX][curY + 2] = IS_FILLED;
-                                    tabCases[curX][curY + 1] = IS_FILLED;
-                                    break;
-                                case OUEST:
-                                    tabCases[curX][curY] = IS_EMPTY;
-                                    tabCases[curX - 2][curY] = IS_FILLED;
-                                    tabCases[curX - 1][curY] = IS_FILLED;
-                                    break;
+                                switch (k) {
+                                    case 1:
+                                        tabCases[curX][curY] = IS_EMPTY;
+                                        tabCases[curX][curY - 2] = IS_FILLED;
+                                        tabCases[curX][curY - 1] = IS_FILLED;
+                                        break;
+                                    case 0:
+                                        tabCases[curX][curY] = IS_EMPTY;
+                                        tabCases[curX + 2][curY] = IS_FILLED;
+                                        tabCases[curX + 1][curY] = IS_FILLED;
+                                        break;
+                                    case 3:
+                                        tabCases[curX][curY] = IS_EMPTY;
+                                        tabCases[curX][curY + 2] = IS_FILLED;
+                                        tabCases[curX][curY + 1] = IS_FILLED;
+                                        break;
+                                    case 2:
+                                        tabCases[curX][curY] = IS_EMPTY;
+                                        tabCases[curX - 2][curY] = IS_FILLED;
+                                        tabCases[curX - 1][curY] = IS_FILLED;
+                                        break;
+                                }
+                                p.setTabCases(tabCases);
+                                //System.out.println("retour en arriere.");
+                                //p.printBoard();
                             }
-                            depl.undo(); // todo replace pegs at their previous places
-                           // System.out.println("retour en arriere.");
-                            //p.printBoard();
-                        }
-                    }
 
-                    //prochaine direction
-                    switch (d){
-                        case NORD:
-                            d=Direction.EST;
-                            break;
-                        case EST:
-                            d=Direction.SUD;
-                            break;
-                        case SUD:
-                            d=Direction.OUEST;
-                            break;
-                        case OUEST:
-                            d=Direction.NORD;
-                            break;
                     }
 
                 }
@@ -132,23 +99,23 @@ public class PegLogique {
         return p.getTabCases()[curX][curY] == IS_FILLED;
     }
 
-    public boolean isMovable(int curX, int curY, Direction dir) {
+    public boolean isMovable(int curX, int curY, int k) {
         int[][] tabCases = p.getTabCases();
 
-        switch (dir) {
-            case NORD:
+        switch (k) {
+            case 1:
                 if(curY-2 < 0)
                     return false;
                 return tabCases[curX][curY] == IS_EMPTY && tabCases[curX][curY-2] == IS_FILLED && tabCases[curX][curY-1] == IS_FILLED;
-            case EST:
+            case 0:
                 if(curX+2 >= tabCases.length)
                     return false;
                 return tabCases[curX][curY] == IS_EMPTY && tabCases[curX+2][curY] == IS_FILLED && tabCases[curX+1][curY] == IS_FILLED;
-            case SUD:
+            case 3:
                 if(curY+2 >= tabCases.length)
                     return false;
                 return tabCases[curX][curY] == IS_EMPTY && tabCases[curX][curY+2] == IS_FILLED && tabCases[curX][curY+1] == IS_FILLED;
-            case OUEST:
+            case 2:
                 if(curX-2 < 0)
                     return false;
                 return tabCases[curX][curY] == IS_EMPTY && tabCases[curX-2][curY] == IS_FILLED && tabCases[curX-1][curY] == IS_FILLED;
